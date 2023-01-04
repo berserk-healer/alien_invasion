eb39d097acc0a090e9c4ca015685b6af6b90f4df
berserk-healer <dandereperson@gmail.com>
2023-01-04 23:00:41 +0700

    base game

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to store all settings for Alien Invasions."""
+
+    def __init__(self):
+        """Initialize game settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+        self.ship_speed_factor = 1.5
