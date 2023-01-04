c9b76e1d781de267a38d2e267d41fa16104490eb
berserk-healer <dandereperson@gmail.com>
2023-01-05 02:03:42 +0700

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,28 +1,50 @@
 import sys
 import pygame
+from bullets import Bullet
 
-def check_events(ship):
+
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypress and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
             
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     screen.fill(ai_settings.bg_color)
-    ship.blitme()
-    ship.update()
+    ship.blit_me()
     pygame.display.flip()
+    # Redraw bullets behind the ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
 
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    # Move the ship with arrow keys
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
